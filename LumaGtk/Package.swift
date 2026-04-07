dc148bd88827ec61c2719dc5b0d619034989abc3
Ole André Vadla Ravnås <oleavr@frida.re>
2026-04-07 19:11:27 +0200

    luma-gtk: Use local SwiftGtk fork checkouts
    Switches the SwiftGtk dependency chain from a single git URL to
    explicit path overrides into the local swift-gtk-fork tree, so
    we can patch the bindings in place when we need to.

--- LumaGtk/Package.swift
+++ LumaGtk/Package.swift
@@ -9,7 +9,22 @@ let package = Package(
     ],
     dependencies: [
         .package(path: ".."),
-        .package(url: "https://github.com/rhx/SwiftGtk", branch: "gtk4"),
+        .package(name: "gir2swift", path: "../../swift-gtk-fork/gir2swift"),
+        .package(name: "SwiftLibXML", path: "../../swift-gtk-fork/SwiftLibXML"),
+        .package(name: "SwiftGLib", path: "../../swift-gtk-fork/SwiftGLib"),
+        .package(name: "SwiftGModule", path: "../../swift-gtk-fork/SwiftGModule"),
+        .package(name: "SwiftGObject", path: "../../swift-gtk-fork/SwiftGObject"),
+        .package(name: "SwiftGIO", path: "../../swift-gtk-fork/SwiftGIO"),
+        .package(name: "SwiftCairo", path: "../../swift-gtk-fork/SwiftCairo"),
+        .package(name: "SwiftHarfBuzz", path: "../../swift-gtk-fork/SwiftHarfBuzz"),
+        .package(name: "SwiftPango", path: "../../swift-gtk-fork/SwiftPango"),
+        .package(name: "SwiftPangoCairo", path: "../../swift-gtk-fork/SwiftPangoCairo"),
+        .package(name: "SwiftGdkPixbuf", path: "../../swift-gtk-fork/SwiftGdkPixbuf"),
+        .package(name: "SwiftGraphene", path: "../../swift-gtk-fork/SwiftGraphene"),
+        .package(name: "SwiftAtk", path: "../../swift-gtk-fork/SwiftAtk"),
+        .package(name: "SwiftGdk", path: "../../swift-gtk-fork/SwiftGdk"),
+        .package(name: "SwiftGsk", path: "../../swift-gtk-fork/SwiftGsk"),
+        .package(name: "SwiftGtk", path: "../../swift-gtk-fork/SwiftGtk"),
     ],
     targets: [
         .systemLibrary(
